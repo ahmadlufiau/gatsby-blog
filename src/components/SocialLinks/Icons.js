7c71c5cd26af8333fd103f4dc0dccac7cc2978ac
Ahmad Lufi A U <ahmadlufi300@gmail.com>
2019-12-20 13:29:35 +0700

    Create Icons.js

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from 'styled-icons/feather/Github'
+import { Instagram } from 'styled-icons/feather/Instagram'
+import { Linkedin } from 'styled-icons/feather/Linkedin'
+import { Codepen } from 'styled-icons/feather/Codepen'
+
+const Icons = {
+  Github,
+  Linkedin,
+  Instagram,
+  Codepen,
+}
+
+export default Icons
