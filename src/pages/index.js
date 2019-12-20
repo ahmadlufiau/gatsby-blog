0e049e5cd5b3a9ba8e0e4d31bccf292bbf6932a6
Ahmad Lufi A U <ahmad@nextbase.co>
2019-12-20 13:54:24 +0700

    back layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Layout from '../components/Layout'
 import media from 'styled-media-query'
 import SEO from '../components/seo'
 import Recommendations from '../components/Recommendations'
