0c9ddcd38c1f4065b7f9fe936236f1fe7378bdcd
Ahmad Lufi A U <ahmad@nextbase.co>
2019-12-20 13:43:59 +0700

    change layout

---
+++ src/components/Layout/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import media from 'styled-media-query'
+
+export const LayoutWrapper = styled.section``
+
+export const LayoutMain = styled.main`
+  background: #fff;
+  min-height: 100vh;
+  padding: 2rem 0 0 270px;
+  ${media.lessThan('large')`
+    padding: 68px 1rem 1rem;
+  `}
+`
+
+export const LayoutMainContent = styled.div`
+  padding: 0 2rem;
+  max-width: 864px;
+  margin: 0 auto;
+  ${media.lessThan('large')`
+    padding: 0;
+  `}
+`
